70c760105990f22c717981030605c6516a112e44
janelleleavins <64174219+janelleleavins@users.noreply.github.com>
2022-01-31 13:39:44 -0800

    Created unit & ability objects
    - Made unit and ability folders within assets
    - Added 7 unit objects
    - Added 7 ability objects
    - Removed basic attack scripts
    - Moved unit and ability scripts to unit folder

--- Dungeon Creepy Crawlers/Assets/Scripts/Unit/Ability.cs
+++ Dungeon Creepy Crawlers/Assets/Scripts/Unit/Ability.cs
@@ -9,8 +9,9 @@ public class Ability : ScriptableObject
     public string description;
 
     public bool teamCast; //for support abilities
+    public bool areaOfEffect; //if it can hit multiple units
 
     public int damage;
     public int range;
-    public int manaCost;
+    public int apCost;
 }
