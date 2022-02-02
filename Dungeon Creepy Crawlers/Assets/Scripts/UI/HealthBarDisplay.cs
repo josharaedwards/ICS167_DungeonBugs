28ad285403dcf45ea100789fa275efa375919dd6
josharaedwards <64117177+josharaedwards@users.noreply.github.com>
2022-02-01 17:53:33 -0800

    Created a Unit prefab and press 'T' to test it working (temporarily)

---
+++ Dungeon Creepy Crawlers/Assets/Scripts/UI/HealthBarDisplay.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarDisplay : MonoBehaviour
+{
+    public Gradient gradient;
+    public Image fill;
+    public int lerpSpeed;
+
+    private float currentAmount;
+    private Slider self;
+
+    public void OnEnable()
+    {
+        self = GetComponent<Slider>();
+        //isInit = false;
+    }
+
+    void Update()
+    {
+        UpdateBar();
+    }
+
+    public void Init(int maxHealth)
+    {
+        self.maxValue = maxHealth;
+        currentAmount = maxHealth;
+        self.value = currentAmount;
+        
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void DamageReceived(int dmg)
+    {
+        currentAmount = self.value - dmg;
+    }
+
+    private void UpdateBar()
+    {
+        if(currentAmount != self.value)
+        {
+            self.value = Mathf.Lerp(currentAmount, self.value, Time.deltaTime * lerpSpeed);
+            fill.color = gradient.Evaluate(self.normalizedValue);
+        }
+    }
+}
