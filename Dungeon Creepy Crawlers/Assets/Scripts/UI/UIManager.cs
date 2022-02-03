bc452966e24cd4be091411cd55fdc2f42114fdff
josharaedwards <64117177+josharaedwards@users.noreply.github.com>
2022-02-02 21:14:16 -0800

    Added "You Win/Lose" text to the UI Manager

--- Dungeon Creepy Crawlers/Assets/Scripts/UI/UIManager.cs
+++ Dungeon Creepy Crawlers/Assets/Scripts/UI/UIManager.cs
@@ -3,12 +3,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager instance;
 
     [SerializeField] private GameObject unitInfoSpawnLoc;
+    [SerializeField] private TextMeshProUGUI winLoseText;
 
     public static UIManager GetInstance()
     {
@@ -27,8 +30,30 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowWinLoseText(false);
+        }
+    }
+
     public Transform GetUnitSpawnLocation()
     {
         return unitInfoSpawnLoc.transform;
+    }
+
+    public void ShowWinLoseText(bool youWon)
+    {
+        if(youWon)
+        {
+            winLoseText.text = "You Won!";
+        }
+        else
+        {
+            winLoseText.text = "You Lose.";
+        }
+
+        winLoseText.gameObject.SetActive(true);
     }
 }
